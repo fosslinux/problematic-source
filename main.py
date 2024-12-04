36aa13e97c8a8fb2e236c9ea9b37911e95da7c16
fosslinux <fosslinux@aussies.space>
2024-12-04 17:20:24 +1100

    MVP :D

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+import sys
+
+from problem import Problem, Severity
+from util import walk_directory
+
+from transform.compressed import CompressedTransformer
+from transform.compressed_rename import CompressedRenameTransformer
+from transform.extract import ExtractTransformer
+
+# Ordered list of transforms
+_transforms = [CompressedRenameTransformer(), CompressedTransformer(), ExtractTransformer()]
+
+from checks.mime import MimeChecker
+
+# List of checks
+_checks = [MimeChecker()]
+
+def transforms(directory: str) -> dict[str, list[Problem]]:
+    # The first pass runs the first transform
+    # This is repeated until it is a no-op
+    # Then, the second pass runs the first two transforms
+    # Repeated until it is a no-op, etc.
+    problems = {}
+    for i in range(len(_transforms)):
+        action = True 
+        while action:
+            action = False
+            for transform in _transforms[:i + 1]:
+                for file in walk_directory(directory):
+                    acted, problem = transform.execute(file)
+                    if acted:
+                        action = True
+                    if problem:
+                        if file not in problems:
+                            problems[file] = [problem]
+                        elif problem not in problems[file]:
+                            problems[file].append(problem)
+    return problems
+
+def checks(directory: str) -> dict[str, list[Problem]]:
+    problems = {}
+    for file in walk_directory(directory):
+        file_problems = []
+        for check in _checks:
+            problem = check.execute(file)
+            if problem:
+                file_problems.append(problem)
+        if file_problems != []:
+            problems[file] = file_problems
+    return problems
+
+def main():
+    outdir = sys.argv[1]
+    os.makedirs(outdir, exist_ok=True)
+    for arg in sys.argv[2:]:
+        dest = os.path.join(outdir, os.path.basename(arg))
+        if os.path.isdir(arg):
+            shutil.copytree(arg, dest)
+        elif os.path.isfile(arg):
+            shutil.copyfile(arg, dest)
+
+    transform_problems = transforms(outdir)
+    print("The following problems were encountered while transforming files to be checked:")
+    for file, problems in transform_problems.items():
+        for problem in problems:
+            problem.print(file)
+    print()
+
+    print("The follow possible problems were found:")
+    check_problems = checks(outdir)
+    for file, problems in check_problems.items():
+        for problem in problems:
+            problem.print(file)
+
+if __name__ == "__main__":
+    main()
--- problem.py
+++ problem.py
@@ -1,12 +1,37 @@
 import enum
 
 class Severity(enum.Enum):
-    FATAL = enum.auto
-    ERROR = enum.auto
-    WARN = enum.auto
-    UNSUPPORTED = enum.auto
+    FATAL = 1
+    ERROR = 2
+    WARN = 3
+    UNSUPPORTED = 4
 
 class Problem():
     def __init__(self, severity: Severity, desc: str):
         self.severity = severity
         self.desc = desc
+
+    def __eq__(self, other):
+        if not isinstance(other, Problem):
+            return NotImplemented
+        return self.severity == other.severity and self.desc == other.desc
+    
+    def _print_type(self, color: str, icon: str, file: str):
+        RESET_COLOR = "\033[0m"
+        print(f"{color}{icon} {file}: {self.desc}{RESET_COLOR}")
+
+    def print(self, file: str):
+        FATAL_COLOR = "\033[91m"
+        ERROR_COLOR = "\033[41m"
+        WARN_COLOR = "\033[33m"
+        UNSUPPORTED_COLOR = "\033[90m"
+
+        match self.severity:
+            case Severity.FATAL:
+                self._print_type(FATAL_COLOR, "\u2620", file)
+            case Severity.ERROR:
+                self._print_type(ERROR_COLOR, "\u274c", file)
+            case Severity.WARN:
+                self._print_type(WARN_COLOR, "\u26a0", file)
+            case Severity.UNSUPPORTED:
+                self._print_type(UNSUPPORTED_COLOR, "?", file)
