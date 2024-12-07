1d053b470c047151bc634bc719fb38c09e7bffc4
fosslinux <fosslinux@aussies.space>
2024-12-07 18:20:59 +1100

    Add Pod::Man checker

--- checks/test_pod_man.py
+++ checks/test_pod_man.py
@@ -1,11 +1,11 @@
 import os
 
-from checks.help2man import Help2manChecker
+from checks.pod_man import PodManChecker
 from problem import Severity
 
-class TestHelp2manChecker():
-    checker = Help2manChecker(deep=False)
-    path = os.path.join("testdata", "help2man")
+class TestPodManChecker():
+    checker = PodManChecker(deep=False)
+    path = os.path.join("testdata", "pod_man")
 
     def _ok(self, file: str):
         assert self.checker.execute(os.path.join(self.path, file)) == None
@@ -14,7 +14,7 @@ class TestHelp2manChecker():
         problem = self.checker.execute(os.path.join(self.path, file))
         assert problem != None
         assert problem.severity == Severity.ERROR
-        assert "help2man" in problem.desc
+        assert "Pod::Man" in problem.desc
 
     def test_bad1(self):
         self._bad("bad.1")
